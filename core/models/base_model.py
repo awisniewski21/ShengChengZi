Fix image normalization

core/models/base_model.py
@@ -1,9 +1,12 @@
+import pickle
 import sys
 from abc import ABC, abstractmethod
+from collections import defaultdict
 from datetime import datetime
 from pathlib import Path
 from typing import Dict, List, Tuple
 
+import numpy as np
 import torch
 import torch.nn as nn
 from matplotlib import pyplot as plt
@@ -61,13 +64,15 @@ class TrainModelBase(ABC):
 
             # Training epoch
             train_metrics = self.train_epoch()
+            train_metrics = {k: np.mean(v) for k, v in train_metrics.items()} # Average metrics
             self.log_metrics(train_metrics, self.current_epoch, "train")
             print(f"Epoch {epoch}/{self.config.num_epochs}: {train_metrics}")
 
             # Validation epoch
             val_loss = None
             if self.val_dataloader is not None and epoch % self.config.eval_epoch_interval == 0:
-                val_metrics = self.eval_epoch("val")
+                val_metrics, _ = self.eval_epoch("val")
+                val_metrics = {k: np.mean(v) for k, v in val_metrics.items()} # Average metrics
                 self.log_metrics(val_metrics, self.current_epoch, "val")
                 val_loss = val_metrics["loss"]
                 print(f"Validation at epoch {epoch}: {val_metrics}")
@@ -84,19 +89,18 @@ class TrainModelBase(ABC):
         Train the model for one epoch and return metrics
         """
         self.net.train()
-        epoch_metrics = {"loss": 0.0, "count": 0}
+        epoch_metrics = defaultdict(list)
 
         pbar = tqdm(self.train_dataloader, desc=f"Epoch {self.current_epoch}")
         for batch_data in pbar:
             # Forward pass, loss computation, and backward pass
             loss = self.train_step(batch_data)
 
-            epoch_metrics["loss"] += loss
-            epoch_metrics["count"] += 1
+            epoch_metrics["loss"].append(loss)
+            epoch_metrics["learning_rate"].append(self.optimizer.param_groups[0]["lr"])
             self.global_step += 1
 
-            avg_loss = epoch_metrics["loss"] / epoch_metrics["count"]
-            pbar.set_postfix({"loss": f"{avg_loss:.4f}"})
+            pbar.set_postfix({"loss": f"{np.mean(epoch_metrics['loss']):.4f}"})
 
             if self.global_step % self.config.log_step_interval == 0:
                 self.writer.add_scalar("train_step/loss", loss, self.global_step)
@@ -104,10 +108,7 @@ class TrainModelBase(ABC):
                     self.writer.add_scalar("train_step/learning_rate", self.optimizer.param_groups[0]["lr"], self.global_step)
                 self.writer.flush()
 
-        return {
-            "loss": epoch_metrics["loss"] / epoch_metrics["count"],
-            "learning_rate": self.optimizer.param_groups[0]["lr"]
-        }
+        return epoch_metrics
 
     @abstractmethod
     def train_step(self, batch_data) -> float:
@@ -117,12 +118,13 @@ class TrainModelBase(ABC):
         """
         pass
 
-    def eval_epoch(self, phase: str) -> Dict:
+    def eval_epoch(self, phase: str) -> Tuple[Dict, List]:
         """
-        Evaluate the model for one epoch and return metrics
+        Evaluate the model for one epoch and return the results
         """
         self.net.eval()
-        eval_metrics = {"loss": 0.0, "count": 0}
+        eval_metrics = defaultdict(list)
+        eval_results = defaultdict(list)
 
         if phase.lower() == "val":
             dataloader = self.val_dataloader
@@ -135,24 +137,28 @@ class TrainModelBase(ABC):
 
         with torch.no_grad():
             for batch_ix, batch_data in tqdm(enumerate(dataloader), desc=desc):
-                loss, out_grid_img, out_labels = self.eval_step(batch_data, phase)
-                eval_metrics["loss"] += loss
-                eval_metrics["count"] += 1
+                loss, out_grid_img, out_labels, metrics, info = self.eval_step(batch_data, phase)
+                eval_metrics["loss"].append(loss)
+                for key, value in metrics.items():
+                    eval_metrics[key].append(value)
+                eval_results["out_grid_imgs"].append(out_grid_img)
+                eval_results["out_labels"].append(out_labels)
+                eval_results["info"].append(info)
 
                 if batch_ix == 0 or phase.lower() == "test":
                     self._log_image_grid(out_grid_img, phase, batch_ix, out_labels=out_labels)
 
-        return {"loss": eval_metrics["loss"] / eval_metrics["count"]}
+        return eval_metrics, eval_results
 
     @abstractmethod
-    def eval_step(self, batch_data, phase: str) -> Tuple[float, torch.Tensor]:
+    def eval_step(self, batch_data, phase: str) -> Tuple[float, torch.Tensor, List[str] | None, Dict, Dict]:
         """
         Perform a single evaluation step on a batch of data
         Includes forward pass, loss computation, and logging output images
         """
         pass
 
-    def test(self):
+    def test(self) -> Tuple[Dict, Dict]:
         """
         Run inference on the model using the test dataset and log the results
         """
@@ -160,14 +166,15 @@ class TrainModelBase(ABC):
 
         assert self.test_dataloader is not None, "Test dataloader must be defined for testing"
         self.writer = SummaryWriter(log_dir=str(self.log_dir))
-
-        test_metrics = self.eval_epoch("test")
-        self.log_metrics(test_metrics, self.current_epoch, "test")
-        print(f"Test metrics: {test_metrics}")
-
+        test_metrics, test_results = self.eval_epoch("test")
         self.writer.close()
+        with open(self.log_dir / "test_metrics.pkl", "wb") as f:
+            pickle.dump(test_metrics, f)
+        with open(self.log_dir / "test_results.pkl", "wb") as f:
+            pickle.dump(test_results, f)
+        return test_metrics, test_results
 
-    def inference(self, input_data):
+    def inference(self, input_data) -> Tuple[torch.Tensor, List[str] | None, Dict]:
         """
         Run inference on the model using the provided input data
         """
@@ -177,10 +184,11 @@ class TrainModelBase(ABC):
 
         self.net.eval()
         with torch.no_grad():
-            out_grid_img, out_labels = self.inference_step(input_data)
+            out_grid_img, out_labels, info = self.inference_step(input_data)
             self._log_image_grid(out_grid_img, "inference", 0, out_labels=out_labels)
+        return out_grid_img, out_labels, info
 
-    def inference_step(self, input_data) -> Tuple[torch.Tensor, List[str] | None]:
+    def inference_step(self, input_data) -> Tuple[torch.Tensor, List[str] | None, Dict]:
         """
         Perform a single inference step on input data
         Includes forward pass and logging output images

core/models/c2cbi_cyclegan.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 
 from core.configs import TrainConfig_C2CBi_CycleGAN
 from core.models import TrainModelBase
-from core.utils.image_utils import make_image_grid, to_out_img
+from core.utils.image_utils import compute_image_metrics, make_image_grid, to_out_img
 from cyclegan_and_pix2pix.cyclegan_network import CycleGANNetwork, CycleGANOutput  # NOQA
 from cyclegan_and_pix2pix.image_pool import ImagePool
 from cyclegan_and_pix2pix.networks import GANLoss
@@ -64,42 +64,41 @@ class TrainModel_C2CBi_CycleGAN(TrainModelBase):
 
         return loss_G.item()
 
-    def eval_step(self, batch_data: Tuple[torch.Tensor, torch.Tensor], phase: str) -> Tuple[float, torch.Tensor, List[str] | None]:
-        src_imgs, trg_imgs = batch_data
-        real_A = src_imgs.to(self.device)
-        real_B = trg_imgs.to(self.device)
+    def eval_step(self, batch_data: Tuple[torch.Tensor, torch.Tensor], phase: str) -> Tuple[float, torch.Tensor, List[str] | None, Dict]:
+        real_A, real_B = batch_data
+        real_A = real_A.to(self.device)
+        real_B = real_B.to(self.device)
 
-        with torch.no_grad():
-            out = self.net.forward(real_A, real_B)
-            eval_loss = (self.loss_cycle(out.rec_A, real_A) + self.loss_cycle(out.rec_B, real_B)) / 2
+        out = self.net.forward(real_A, real_B)
+
+        eval_loss = (self.loss_cycle(out.rec_A, real_A) + self.loss_cycle(out.rec_B, real_B)) / 2
 
         real_A_out = to_out_img(real_A, (0, 1))
-        fake_B_out = to_out_img(out.fake_B, (-1, 1))
-        rec_A_out = to_out_img(out.rec_A, (-1, 1))
+        fake_B_out = to_out_img(out.fake_B, (0, 1))
+        rec_A_out = to_out_img(out.rec_A, (0, 1))
         real_B_out = to_out_img(real_B, (0, 1))
-        fake_A_out = to_out_img(out.fake_A, (-1, 1))
-        rec_B_out = to_out_img(out.rec_B, (-1, 1))
+        fake_A_out = to_out_img(out.fake_A, (0, 1))
+        rec_B_out = to_out_img(out.rec_B, (0, 1))
+        grid_img = make_image_grid([real_A_out, fake_B_out, rec_A_out, real_B_out, fake_A_out, rec_B_out])
 
-        grid_img = make_image_grid([
-            real_A_out, fake_B_out, rec_A_out,
-            real_B_out, fake_A_out, rec_B_out
-        ])
+        metrics = {"A": compute_image_metrics(out.rec_A, real_A), "B": compute_image_metrics(out.rec_B, real_B)}
+        metrics = {k: (metrics["A"][k] + metrics["B"][k]) / 2 for k in metrics["A"].keys()}
+        info = {"real_A": real_A, "fake_B": out.fake_B, "rec_A": out.rec_A, "real_B": real_B, "fake_A": out.fake_A, "rec_B": out.rec_B}
 
-        return eval_loss.item(), grid_img, None
+        return eval_loss.item(), grid_img, None, metrics, info
 
-    def inference_step(self, input_data: torch.Tensor) -> Tuple[torch.Tensor, List[str] | None]:
+    def inference_step(self, input_data: torch.Tensor) -> Tuple[torch.Tensor, List[str] | None, Dict]:
         real_AB = input_data.to(self.device)
 
-        with torch.no_grad():
-            fake_B = self.net.netG_A(real_AB) # input -> B
-            fake_A = self.net.netG_B(real_AB) # input -> A
+        fake_B = self.net.netG_A(real_AB) # input -> B
+        fake_A = self.net.netG_B(real_AB) # input -> A
 
         real_AB_out = to_out_img(real_AB, (0, 1))
         fake_B_out = to_out_img(fake_B, (-1, 1))
         fake_A_out = to_out_img(fake_A, (-1, 1))
         grid_img = make_image_grid([real_AB_out, fake_B_out, fake_A_out])
 
-        return grid_img, None
+        return grid_img, None, {"real_AB": real_AB, "fake_B": fake_B, "fake_A": fake_A}
 
     def get_checkpoint_data(self) -> Dict:
         chkpt_data = super().get_checkpoint_data()

core/models/t2c_glyffuser.py
@@ -7,7 +7,7 @@ from diffusers.utils.torch_utils import randn_tensor
 
 from core.configs import TrainConfig_T2C_Glyff
 from core.models import TrainModelBase
-from core.utils.image_utils import make_image_grid, to_out_img
+from core.utils.image_utils import compute_image_metrics, make_image_grid, to_out_img
 
 
 class TrainModel_T2C_Glyffuser(TrainModelBase):
@@ -66,12 +66,15 @@ class TrainModel_T2C_Glyffuser(TrainModelBase):
         eval_loss = torch.nn.functional.mse_loss(pred_imgs, trg_imgs)
 
         trg_imgs_out = to_out_img(trg_imgs, (0, 1))
-        pred_imgs_out = to_out_img(pred_imgs, (-1, 1))
+        pred_imgs_out = to_out_img(pred_imgs, (0, 1))
         grid_img = make_image_grid([pred_imgs_out, trg_imgs_out])
 
-        return eval_loss.item(), grid_img, src_texts_raw
+        metrics = compute_image_metrics(pred_imgs, trg_imgs)
+        info = {"trg_imgs": trg_imgs, "pred_imgs": pred_imgs, "src_texts": src_texts_raw}
 
-    def inference_step(self, input_data: Tuple[torch.Tensor, torch.Tensor, List[str]]) -> Tuple[torch.Tensor, List[str] | None]:
+        return eval_loss.item(), grid_img, src_texts_raw, metrics, info
+
+    def inference_step(self, input_data: Tuple[torch.Tensor, torch.Tensor, List[str]]) -> Tuple[torch.Tensor, List[str] | None, Dict]:
         inference_pipeline = DiffusionPipeline_T2C_Glyff(unet=self.net, scheduler=self.inference_scheduler)
         inference_pipeline.set_progress_bar_config(desc="Generating inference image grid...")
 
@@ -88,21 +91,21 @@ class TrainModel_T2C_Glyffuser(TrainModelBase):
             output_type="numpy",
         ).images
 
-        pred_imgs_out = to_out_img(pred_imgs, (-1, 1))
+        pred_imgs_out = to_out_img(pred_imgs, (0, 1))
         grid_img = make_image_grid([pred_imgs_out])
 
-        return grid_img, src_texts_raw
+        return grid_img, src_texts_raw, {"pred_imgs": pred_imgs, "src_texts": src_texts_raw}
 
     def get_checkpoint_data(self) -> Dict:
         chkpt_data = super().get_checkpoint_data()
-        chkpt_data["noise_scheduler_state"] = self.noise_scheduler
-        chkpt_data["inference_scheduler_state"] = self.inference_scheduler
+        chkpt_data["noise_scheduler_state"] = dict(self.noise_scheduler.config)
+        chkpt_data["inference_scheduler_state"] = dict(self.inference_scheduler.config)
         return chkpt_data
 
     def load_checkpoint_data(self, chkpt_data: Dict, phase: str):
         super().load_checkpoint_data(chkpt_data, phase)
-        self.noise_scheduler = chkpt_data["noise_scheduler_state"]
-        self.inference_scheduler = chkpt_data["inference_scheduler_state"]
+        self.noise_scheduler = DDPMScheduler.from_config(chkpt_data["noise_scheduler_state"])
+        self.inference_scheduler = DPMSolverMultistepScheduler.from_config(chkpt_data["inference_scheduler_state"])
 
 
 class DiffusionPipeline_T2C_Glyff(DDPMPipeline):

palette/palette_network.py
@@ -168,9 +168,10 @@ class PaletteNetwork(nn.Module):
         for i in tqdm(reversed(range(0, self.num_timesteps)), total=self.num_timesteps):
             t = torch.full((b,), i, device=y_cond.device, dtype=torch.long)
             y_t = self.p_sample(y_t, t, y_cond=y_cond)
+            y_t_out = ((y_t + 1) / 2).clamp(0, 1)
             if i % sample_inter == 0:
-                ret_arr = torch.cat([ret_arr, y_t], dim=0)
-        return y_t, ret_arr
+                ret_arr = torch.cat([ret_arr, y_t_out], dim=0)
+        return y_t_out, ret_arr
 
     def forward(self, y_0: torch.Tensor, y_cond: torch.Tensor | None = None, noise: torch.Tensor | None = None) -> float:
         # Sample from p(gammas)